1965f612e9ea16d5dcf97e1bf669f4984f455f3e
Jeff Johnston <jeffwjohn@yahoo.com>
2020-08-26 13:20:23 -0500

    take over

--- assets/js/script.js
+++ assets/js/script.js
@@ -283,7 +283,14 @@ var auditTask = function (taskEl) {
   } else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
+  // console.log(taskEl);
 };
+
+setInterval(function () {
+  $(".card .list-group-item").each(function (el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
 
 // load tasks for the first time
 loadTasks();
